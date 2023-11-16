3b300fcb5c2ecc408ede43983608954df0e5d65d
Bushra jabin <bushrabintjabin@gmail.com>
2023-11-16 11:30:11 +0530

    Added font awsome icon in input field

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,11 @@
 import "./Contact.css"
 const Contact = () => {
-
-    const SubmitForm=()=>{
+    const SubmitForm = () => {
         // alert("hii")
 
     }
     return (
+
         <div className="contact_page">
             <div className="contact_top_title">
                 <h2>Reach US Out</h2>
@@ -13,9 +13,10 @@ const Contact = () => {
             </div>
 
             <div className="contact_input_field">
-                <input type="text" placeholder="Name" />
-                <input type="email" placeholder="Email" />
+                < i class="fa fa -solid fa-user" /><input type="text" placeholder="Name" />
+                <i class="fa fa-solid fa-envelope"></i><input type="email" placeholder="Email" />
                 <input type="number" />
+
                 <select id="options" >
                     <option value="select">Select Services </option>
                     <option value="Major service">Major service</option>
@@ -23,10 +24,11 @@ const Contact = () => {
                     <option value="Mobile App  Development">Mobile App  Development</option>
                     <option value="Designing Services">Designing Services</option>
                     <option value="igital Marketing">Digital Marketing</option>
-                </select>
-                <textarea name="" id="text" cols="100" rows="1" placeholder="Note"></textarea><br></br>
+                </select><br></br>
+                <i class="fa fa-file-text-o" i /><textarea name="" id="text" cols="100" rows="1" placeholder="Note"></textarea><br></br>
                 <button className="submitButton" onClick={SubmitForm}>SUBMIT</button>
             </div>
+
         </div>
 
 
